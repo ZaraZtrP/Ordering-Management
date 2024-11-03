097aa4f0dd9950fa2afcfeadc5debf68fcee4b26
ZaraZP <116828633+ZaraZP@users.noreply.github.com>
2024-11-03 18:17:24 +0700

    Initial commit

--- script.js
+++ script.js
@@ -14,13 +14,19 @@ document.getElementById('orderForm').addEventListener('submit', function (e) {
                     `Detail Pesanan: ${order}`;
 
     // Encode URL untuk pengiriman
-    const whatsappUrl = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
+    const whatsappUrl = `https://wa.me/+6283182068209?text=${encodeURIComponent(message)}`;
 
     // Buka WhatsApp dengan URL yang sudah diencode
     window.open(whatsappUrl, '_blank');
 
-    // Tampilkan pesan konfirmasi
+    // Tampilkan pesan konfirmasi di halaman
     const confirmationMessage = document.getElementById('confirmationMessage');
     confirmationMessage.style.display = 'block';
-    confirmationMessage.innerHTML = `Pesanan Anda telah terkirim ke nomor <strong>${phone}</strong> dengan detail sebagai berikut:<br><br><strong>${order}</strong>`;
+    confirmationMessage.innerHTML = `Pesanan Anda terkirim ke Mas X dengan rincian sebagai berikut:<br><br>` +
+                                    `<strong>Nama:</strong> ${name}<br>` +
+                                    `<strong>Nomor Telepon:</strong> ${phone}<br>` +
+                                    `<strong>Detail Pesanan:</strong> ${order}`;
+
+    // Reset form setelah pengiriman
+    document.getElementById('orderForm').reset();
 });

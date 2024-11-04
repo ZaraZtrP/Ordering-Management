cf04b4860c560a9d9acf61fb1603e5fc75aa5e98
ZaraZtrP <zarazputi@gmail.com>
2024-11-04 09:57:03 +0700

    update isi form

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ document.getElementById('orderForm').addEventListener('submit', function (e) {
 
     // Ambil data dari form
     const name = document.getElementById('name').value;
+    const divisi = document.getElementById('divisi').value;
     const phone = document.getElementById('phone').value.trim(); // Menghapus spasi
     const order = document.getElementById('order').value;
+    const ruang = document.getElementById('ruang').value;
 
     // Validasi format nomor telepon
     const phonePattern = /^\+62[0-9]{10,15}$/;
@@ -15,10 +17,12 @@ document.getElementById('orderForm').addEventListener('submit', function (e) {
     }
 
     // Format pesan untuk WhatsApp
-    const message = `Halo, saya ingin memesan kebutuhan karyawan berikut:\n\n` +
+    const message = `Halo, saya ingin memesan kebutuhan berikut:\n\n` +
                     `Nama: ${name}\n` +
-                    `Nomor Telepon: ${phone}\n` +
-                    `Detail Pesanan: ${order}`;
+                    `dari divisi: ${divisi}`+
+                    `Detail Pesanan: ${order}`+
+                    `Untuk diantarkan ke: ${ruang}`+
+                    `Nomor Telepon: ${phone}\n` ;
 
     // Encode URL untuk pengiriman
     const whatsappUrl = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
@@ -29,5 +33,5 @@ document.getElementById('orderForm').addEventListener('submit', function (e) {
     // Tampilkan pesan konfirmasi
     const confirmationMessage = document.getElementById('confirmationMessage');
     confirmationMessage.style.display = 'block';
-    confirmationMessage.innerHTML = `Pesanan Anda telah terkirim ke nomor <strong>${phone}</strong> dengan detail sebagai berikut:<br><br><strong>${order}</strong>`;
+    confirmationMessage.innerHTML = `Pesanan Anda telah terkirim </strong> dengan detail sebagai berikut:<br><br><strong>${order}</strong> dan akan segera diantar ke <strong>${ruang}</strong>`;
 });
